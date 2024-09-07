6fa3ba8f5e45bef098ce76e65749142bbd793fcd
MauricioStrang <strangmauricio@gmail.com>
2024-09-07 15:51:49 -0300

    class names

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -7,32 +7,32 @@ export default function Header() {
             <button>Burger menu</button>
         </div>
 
-        <div>
+        <div className={styles.logo}>
             <h1>MORDEXtm</h1>
         </div>
 
-        <div>
+        <div className={styles.logo}>
           <button>Categorias dropdown</button>
         </div>
 
-        <div>
+        <div className={styles.searchContainer}>
             <div className={styles.searchBar}>
                 <p>¿Qué estás buscando?</p>
             </div>
         </div>
 
 
-        <div>
+        <div className={styles.searchLogo}>
             <button>Search button</button>
         </div>
 
 
-        <div>
+        <div className={styles.userLogo}>
             <button>Login logo</button>
             <p>Login</p>
         </div>
 
-        <div>
+        <div className={styles.cartLogo}>
             <button>Cart logo</button>
         </div>
     </div>
