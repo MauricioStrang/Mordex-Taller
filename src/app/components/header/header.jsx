c5cab65abbe2bd255ad83ae74bc8d4b3be168cb9
MauricioStrang <strangmauricio@gmail.com>
2024-09-08 21:41:52 -0300

    working on header

--- src/app/components/header/header.jsx
+++ src/app/components/header/header.jsx
@@ -1,30 +1,42 @@
+import Image from "next/image";
 import styles from "./header.module.css" 
 
 export default function Header() {
   return (
+
     <div className={styles.container}>
-        <div>
-            <button>Burger menu</button>
+
+        <div className={styles.brgMenuContainer}>
+            <Image className={styles.brgMenu} src={'/menu.png'} width={25} height={20} alt="menu button"/>
         </div>
 
-        <div className={styles.logo}>
-            <h1>MORDEXtm</h1>
+        <div className={styles.logoContainer}>
+            <h1 className={styles.logo}> Mordex Taller </h1>
         </div>
 
-        <div className={styles.logo}>
-          <button>Categorias dropdown</button>
-        </div>
 
         <div className={styles.searchContainer}>
-            <div className={styles.searchBar}>
-                <p>¿Qué estás buscando?</p>
+            <div className={styles.dropdown}>
+                <select name="Categorías" id="cars">
+                    <option value="volvo">Todas las Categorías</option>
+                    <option value="volvo">Remeras</option>
+                    <option value="saab">Herramientas</option>
+                    <option value="opel">Sticker</option>
+                </select>
+            </div>
+
+            <div className={styles.searchContainer}>
+                <div className={styles.searchBar}>
+                    <p>¿Qué estás buscando?</p>
+                </div>
+            </div>
+
+
+            <div className={styles.searchLogo}>
+                <button>Search button</button>
             </div>
         </div>
-
-
-        <div className={styles.searchLogo}>
-            <button>Search button</button>
-        </div>
+        
 
 
         <div className={styles.userLogo}>
