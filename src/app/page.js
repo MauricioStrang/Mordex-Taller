f6d111f198b95090b94577bd211d1bf66009d767
MauricioStrang <strangmauricio@gmail.com>
2024-10-15 11:57:33 -0300

    fixed main banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+export const metadata = {
+  description: "Home page",
+};
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
         <div className={styles.mordexBannerContainer}>
           <Image
-          className={styles.mordexBanner} 
-          src="/mordexfix.png"
+          src='/mordexfix.png'
+          alt=''
           fill
+          className={styles.mordexBanner} 
           />
+        </div>
+
+        <div className={styles.mainContent}>
+          <div className={styles.links}>
+              <h1>Hello World</h1>
+          </div>
         </div>
         
     </div>
