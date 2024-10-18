a2c9a448dd4ecad4ee26c0195c1b22b862c79878
MauricioStrang <strangmauricio@gmail.com>
2024-10-17 21:17:09 -0300

    links icons added

--- src/app/page.js
+++ src/app/page.js
@@ -22,19 +22,44 @@ export default function Home() {
           <div className={styles.links}>
 
               <div className={styles.aboutContainer}>
-
+                <div className={styles.imgContainer}>
+                  <Image 
+                  src='/people-outline.svg'
+                  fill
+                  className={styles.aboutImg}
+                  />
+                </div>
+                  
               </div>
 
               <div className={styles.storeContainer}>
-                
+                <div className={styles.imgContainer}>
+                    <Image 
+                    src='/store.svg'
+                    fill
+                    className={styles.storeImg}
+                    />
+                </div>
               </div>
 
               <div className={styles.businessContainer}>
-                
+                <div className={styles.imgContainer}>
+                      <Image 
+                      src='/business.svg'
+                      fill
+                      className={styles.businessImg}
+                      />
+                </div>
               </div>
 
               <div className={styles.repairsContainer}>
-                
+                <div className={styles.imgContainer}>
+                      <Image 
+                      src='/repairs.svg'
+                      fill
+                      className={styles.repairsImg}
+                      />
+                </div>
               </div>
           </div>
         </div>
